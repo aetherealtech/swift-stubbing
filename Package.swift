55b309afe12d06ad9fa6a042865afd3aba75010f
Dan Coleman <dan@aetherealtech.com>
2024-10-08 06:25:43 -0500

    Update packages

--- Package.swift
+++ Package.swift
@@ -14,7 +14,7 @@ let package = Package(
     ],
     dependencies: [
         .package(url: "https://github.com/apple/swift-syntax", from: .init(510, 0, 3)),
-        .package(url: "https://github.com/aetherealtech/swift-assertions", branch: "master"),
+        .package(url: "https://github.com/aetherealtech/swift-assertions", from: .init(0, 1, 0)),
     ],
     targets: [
         .target(
